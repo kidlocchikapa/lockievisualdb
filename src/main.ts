b1fa5a281927715e193c551fc351a3451a393464
kidlocchikapa <bed-com-48-22@unima.ac.mw>
2025-01-13 19:42:47 +0200

    added front end api

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3001'], // Add your frontend URL
+    origin: ['http://localhost:5173', 'http://localhost:3001', 'https://lockievisuals.vercel.app'], // Add your frontend URL
     credentials: true,
   });
   
